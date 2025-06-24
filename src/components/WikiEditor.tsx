47166297ce368cc298ba04a127996c53f98b0c82
Junhyeok <cjh64866@naver.com>
2025-06-25 00:02:13 +0900

    refactor: 안쓰는 링크 코드 제거

--- src/components/WikiEditor.tsx
+++ src/components/WikiEditor.tsx
@@ -2,43 +2,12 @@ import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import { Markdown } from "tiptap-markdown";
 import Image from "@tiptap/extension-image";
-import { Node, nodeInputRule } from "@tiptap/core";
 import MenuBar from "./MenuBar";
 import LinkExtension from "@tiptap/extension-link";
 import TextStyle from "@tiptap/extension-text-style";
 import Color from "@tiptap/extension-color";
 import { InternalLinkPlaceholder } from "../extensions/InternalLinkPlaceholder";
 import { FootnotePlaceholder } from "../extensions/FootnotePlaceholder";
-
-// 1) 커스텀 WikiLink 노드 정의
-const WikiLink = Node.create({
-  name: "wikiLink",
-  group: "inline",
-  inline: true,
-  draggable: false,
-  addAttributes() {
-    return { page: { default: "" } };
-  },
-  parseHTML() {
-    return [{ tag: "a[data-wiki]" }];
-  },
-  renderHTML({ HTMLAttributes }) {
-    return ["a", { "data-wiki": "", href: `/wiki/${HTMLAttributes.page}` }, 0];
-  },
-  addInputRules() {
-    return [
-      // [[Page|Label]] → wikiLink 노드
-      nodeInputRule({
-        find: /\[\[([^\]|]+)(?:\|([^\]]+))?\]\]$/,
-        type: this.type,
-        getAttributes: (match) => ({
-          page: match[1],
-          label: match[2] ?? match[1],
-        }),
-      }),
-    ];
-  },
-});
 
 export default function WikiEditor() {
   const editor = useEditor({
@@ -54,7 +23,6 @@ export default function WikiEditor() {
         html: false, // 화면엔 HTML(렌더링)만, markdown 문법 노출X
       }),
       Image, // /image 업로드·삽입 기능
-      WikiLink, // [[Page|Label]] 커스텀 링크
       InternalLinkPlaceholder,
       FootnotePlaceholder,
     ],
