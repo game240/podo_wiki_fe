ed0bcb29943dda156813a8cc8e34579bec5b0a04
Junhyeok <cjh64866@naver.com>
2025-07-19 23:52:29 +0900

    feat: axios 요청에 supabase token 포함하도록 수정

--- src/apis/axiosClient.ts
+++ src/apis/axiosClient.ts
@@ -1,9 +1,14 @@
 import axios from "axios";
+import { supabase } from "../libs/supabaseClient";
+
+const session = await supabase.auth.getSession();
+const token = session?.data.session?.access_token;
 
 const axiosClient = axios.create({
   baseURL: `${import.meta.env.VITE_SERVER_URL}/api`,
   headers: {
     "Content-Type": "application/json",
+    Authorization: `Bearer ${token}`,
   },
 });
 
