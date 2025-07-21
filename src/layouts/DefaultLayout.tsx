f7f5107dd0c2a3da410acbb6c0382b274e5f8039
Junhyeok <cjh64866@naver.com>
2025-07-22 03:02:37 +0900

    feat: ScrollToTop 컴포넌트 추가

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
-
+import ScrollToTop from "../components/ScrollToTop";
 import NavBar from "../components/NavBar";
 
 const DefaultLayout = () => {
   return (
     <main className="w-screen min-h-screen mx-auto bg-[#F5F5F5]">
       <NavBar />
+      <ScrollToTop />
       <Outlet />
     </main>
   );
